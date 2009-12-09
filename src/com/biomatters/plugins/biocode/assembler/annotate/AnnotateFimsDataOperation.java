9341a1838299980f0086f4cdbc31e936a0dbe9b1
Steven Stones-Havas <steve@biomatters.com>
2009-12-09 00:16:07 +0000

    MBP-82
    git-svn-id: svn+ssh://svn.biomatters.com/usr/local/svnroot/geneious/trunk/plugins/MooreaLabBench@30422 84f1a7b3-ea07-0410-887d-f1e6e76816dd

--- src/com/biomatters/plugins/biocode/assembler/annotate/AnnotateFimsDataOperation.java
+++ src/com/biomatters/plugins/biocode/assembler/annotate/AnnotateFimsDataOperation.java
@@ -2,7 +2,9 @@ package com.biomatters.plugins.biocode.assembler.annotate;
 
 import com.biomatters.geneious.publicapi.documents.AnnotatedPluginDocument;
 import com.biomatters.geneious.publicapi.documents.DocumentField;
+import com.biomatters.geneious.publicapi.documents.PluginDocument;
 import com.biomatters.geneious.publicapi.documents.sequence.NucleotideSequenceDocument;
+import com.biomatters.geneious.publicapi.documents.sequence.SequenceAlignmentDocument;
 import com.biomatters.geneious.publicapi.plugin.*;
 import com.biomatters.plugins.biocode.BiocodePlugin;
 import com.biomatters.plugins.biocode.BiocodeUtilities;
@@ -10,8 +12,7 @@ import com.biomatters.plugins.biocode.labbench.BiocodeService;
 import com.biomatters.plugins.biocode.labbench.ConnectionException;
 import jebl.util.ProgressListener;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author Richard
@@ -32,7 +33,8 @@ public class AnnotateFimsDataOperation extends DocumentOperation {
 
     public DocumentSelectionSignature[] getSelectionSignatures() {
         return new DocumentSelectionSignature[] {
-                new DocumentSelectionSignature(NucleotideSequenceDocument.class, 1, Integer.MAX_VALUE)
+                new DocumentSelectionSignature(NucleotideSequenceDocument.class, 1, Integer.MAX_VALUE),
+                new DocumentSelectionSignature(SequenceAlignmentDocument.class,1, Integer.MAX_VALUE)
         };
     }
 
@@ -70,52 +72,27 @@ public class AnnotateFimsDataOperation extends DocumentOperation {
         progressListener.setIndeterminateProgress();
         progressListener.setMessage("Accessing FIMS...");
         List<String> failBlog = new ArrayList<String>();
+        Set<String> noReferencesList = new LinkedHashSet<String>();
         for (AnnotatedPluginDocument annotatedDocument : annotatedDocuments) {
-            AnnotateFimsDataOptions.FimsData fimsData;
-            fimsData = fimsDataGetter.getFimsData(annotatedDocument);
             if (progressListener.isCanceled()) {
                 break;
             }
-            if (fimsData.fimsSample == null) {
-                failBlog.add(annotatedDocument.getName());
-                continue;
-            }
-
-            for (DocumentField documentField : fimsData.fimsSample.getFimsAttributes()) {
-                annotatedDocument.setFieldValue(documentField, fimsData.fimsSample.getFimsAttributeValue(documentField.getCode()));
-            }
-            annotatedDocument.setFieldValue(BiocodeUtilities.SEQUENCING_PLATE_FIELD, fimsData.sequencingPlateName);
-            annotatedDocument.setFieldValue(BiocodeUtilities.SEQUENCING_WELL_FIELD, fimsData.well.toString());
-            annotatedDocument.setFieldValue(BiocodeUtilities.WORKFLOW_NAME_FIELD, fimsData.workflow.getName());
-            StringBuilder taxonomy = new StringBuilder();
-            String genus = null;
-            String species = null;
-            for (DocumentField documentField : fimsData.fimsSample.getTaxonomyAttributes()) {
-                Object taxon = fimsData.fimsSample.getFimsAttributeValue(documentField.getCode());
-                if (documentField.getName().equalsIgnoreCase("genus")) {
-                    genus = (String)taxon;
+            if(SequenceAlignmentDocument.class.isAssignableFrom(annotatedDocument.getDocumentClass())) {
+                SequenceAlignmentDocument alignment = (SequenceAlignmentDocument)annotatedDocument.getDocument();
+                for(AnnotatedPluginDocument doc : alignment.getReferencedDocuments()) {
+                    if(doc != null) {
+                        annotateDocument(fimsDataGetter, failBlog, doc);
+                    }
+                    else {
+                        noReferencesList.add(doc.getName());
+                    }
                 }
-                if (documentField.getName().toLowerCase().contains("speci")) {
-                    species = (String)taxon;
-                    break;
-                }
-                if (taxon != null) {
-                    taxonomy.append(taxon).append("; ");
-                }
+                copyMatchingFieldsToContig(annotatedDocument);
+                annotatedDocument.saveDocument();
             }
-            if (taxonomy.length() > 0) {
-                annotatedDocument.setFieldValue(DocumentField.TAXONOMY_FIELD, taxonomy.substring(0, taxonomy.length() - 2));
+            else {
+                annotateDocument(fimsDataGetter, failBlog, annotatedDocument);
             }
-            Object organism = annotatedDocument.getFieldValue(DocumentField.ORGANISM_FIELD);
-            if (organism != null && !((String)organism).contains(" ")) {
-                //the database seems to have cases where just the Genus has been entered in the organism column eventhough the species has been entered in the taxonomy columns -> Throw that crap away
-                organism = null;
-                annotatedDocument.setFieldValue(DocumentField.ORGANISM_FIELD, null);
-            }
-            if (organism == null && genus != null && species != null) {
-                annotatedDocument.setFieldValue(DocumentField.ORGANISM_FIELD, genus + " " + species);
-            }
-            annotatedDocument.save();
         }
         if (!failBlog.isEmpty()) {
             StringBuilder b = new StringBuilder("<html>");
@@ -126,6 +103,114 @@ public class AnnotateFimsDataOperation extends DocumentOperation {
             b.append("</html>");
             throw new DocumentOperationException(b.toString());
         }
+        if (!noReferencesList.isEmpty()) {
+            StringBuilder b = new StringBuilder("<html>");
+            b.append("The following contigs could not be annotated because they did not have reference documents:<br><br>");
+            for (String s : noReferencesList) {
+                b.append(s).append("<br>");
+            }
+            b.append("</html>");
+            throw new DocumentOperationException(b.toString());
+        }
+    }
+
+        private static final List<String> FIELDS_TO_NOT_COPY = Arrays.asList(DocumentField.AMBIGUITIES.getCode(),
+            DocumentField.BIN.getCode(), DocumentField.CREATED_FIELD.getCode(), DocumentField.DESCRIPTION_FIELD.getCode(),
+            DocumentField.FIRST_SEQUENCE_RESIDUES.getCode(), DocumentField.HIGH_QUALITY_PERCENT.getCode(), DocumentField.LOW_QUALITY_PERCENT.getCode(),
+            DocumentField.MEDIMUM_QUALITY_PERCENT.getCode(), DocumentField.NAME_FIELD.getCode(), DocumentField.POST_TRIM_LENGTH.getCode(),
+            DocumentField.SEQUENCE_LENGTH.getCode(), DocumentField.TOPOLOGY_FIELD.getCode(), DocumentField.UNREAD_FIELD.getCode(),
+            PluginDocument.MODIFIED_DATE_FIELD.getCode(), "document_size", DocumentField.SEQUENCE_COUNT.getCode());
+
+    /**
+     * copied from AssemblyOperationj
+     * @param annotatedContig
+     * @throws DocumentOperationException
+     */
+    private static void copyMatchingFieldsToContig(AnnotatedPluginDocument annotatedContig) throws DocumentOperationException {
+        SequenceAlignmentDocument contig = (SequenceAlignmentDocument)annotatedContig.getDocument();
+        Map<DocumentField, Object> displayableFieldsToCopy = null;
+        for (int i = 0; i < contig.getNumberOfSequences(); i ++) {
+            if (i == contig.getContigReferenceSequenceIndex()) {
+                continue;
+            }
+            AnnotatedPluginDocument referencedDocument = contig.getReferencedDocument(i);
+            if (referencedDocument == null) {
+                return; //one sequence doesn't have a reference so bail on the whole thing
+            }
+            if (displayableFieldsToCopy == null) {
+                displayableFieldsToCopy = new HashMap<DocumentField, Object>();
+                for (DocumentField field : referencedDocument.getDisplayableFields()) {
+//                    if (field.getCode().startsWith("biocode") || field.getCode().equalsIgnoreCase("tissueid")
+//                            || field.getCode().equals(DocumentField.TAXONOMY_FIELD.getCode())
+//                            || field.getCode().equals(DocumentField.ORGANISM_FIELD.getCode())
+//                            || field.getCode().equals(DocumentField.COMMON_NAME_FIELD.getCode())) {
+//                        displayableFieldsToCopy.put(field, referencedDocument.getFieldValue(field));
+//                    }
+                    if (!FIELDS_TO_NOT_COPY.contains(field.getCode())) {
+                        displayableFieldsToCopy.put(field, referencedDocument.getFieldValue(field));
+                    }
+                }
+            } else {
+                for (Map.Entry<DocumentField, Object> fieldToCopy : new HashSet<Map.Entry<DocumentField, Object>>(displayableFieldsToCopy.entrySet())) {
+                    Object value = referencedDocument.getFieldValue(fieldToCopy.getKey());
+                    if (value == null || !value.equals(fieldToCopy.getValue())) {
+                        displayableFieldsToCopy.remove(fieldToCopy.getKey());
+                    }
+                }
+            }
+            if (displayableFieldsToCopy.isEmpty()) break;
+        }
+        if (displayableFieldsToCopy == null || displayableFieldsToCopy.isEmpty()) return;
+
+        for (Map.Entry<DocumentField, Object> fieldToCopy : displayableFieldsToCopy.entrySet()) {
+            annotatedContig.setFieldValue(fieldToCopy.getKey(), fieldToCopy.getValue());
+        }
+        annotatedContig.save();
+    }
+
+    private static void annotateDocument(FimsDataGetter fimsDataGetter, List<String> failBlog, AnnotatedPluginDocument annotatedDocument) throws DocumentOperationException {
+        AnnotateFimsDataOptions.FimsData fimsData;
+        fimsData = fimsDataGetter.getFimsData(annotatedDocument);
+        if (fimsData.fimsSample == null) {
+            failBlog.add(annotatedDocument.getName());
+            return;
+        }
+
+        for (DocumentField documentField : fimsData.fimsSample.getFimsAttributes()) {
+            annotatedDocument.setFieldValue(documentField, fimsData.fimsSample.getFimsAttributeValue(documentField.getCode()));
+        }
+        annotatedDocument.setFieldValue(BiocodeUtilities.SEQUENCING_PLATE_FIELD, fimsData.sequencingPlateName);
+        annotatedDocument.setFieldValue(BiocodeUtilities.SEQUENCING_WELL_FIELD, fimsData.well.toString());
+        annotatedDocument.setFieldValue(BiocodeUtilities.WORKFLOW_NAME_FIELD, fimsData.workflow.getName());
+        StringBuilder taxonomy = new StringBuilder();
+        String genus = null;
+        String species = null;
+        for (DocumentField documentField : fimsData.fimsSample.getTaxonomyAttributes()) {
+            Object taxon = fimsData.fimsSample.getFimsAttributeValue(documentField.getCode());
+            if (documentField.getName().equalsIgnoreCase("genus")) {
+                genus = (String)taxon;
+            }
+            if (documentField.getName().toLowerCase().contains("speci")) {
+                species = (String)taxon;
+                break;
+            }
+            if (taxon != null) {
+                taxonomy.append(taxon).append("; ");
+            }
+        }
+        if (taxonomy.length() > 0) {
+            annotatedDocument.setFieldValue(DocumentField.TAXONOMY_FIELD, taxonomy.substring(0, taxonomy.length() - 2));
+        }
+        Object organism = annotatedDocument.getFieldValue(DocumentField.ORGANISM_FIELD);
+        if (organism != null && !((String)organism).contains(" ")) {
+            //the database seems to have cases where just the Genus has been entered in the organism column eventhough the species has been entered in the taxonomy columns -> Throw that crap away
+            organism = null;
+            annotatedDocument.setFieldValue(DocumentField.ORGANISM_FIELD, null);
+        }
+        if (organism == null && genus != null && species != null) {
+            annotatedDocument.setFieldValue(DocumentField.ORGANISM_FIELD, genus + " " + species);
+        }
+        annotatedDocument.save();
     }
 
     public static abstract class FimsDataGetter {
