7116bd68b90a472397133d366aad30bd23730930
Steven Stones-Havas <steve@biomatters.com>
2009-12-17 06:29:35 +0000

    moorea - new plugin version
    git-svn-id: svn+ssh://svn.biomatters.com/usr/local/svnroot/geneious/trunk/plugins/MooreaLabBench@30632 84f1a7b3-ea07-0410-887d-f1e6e76816dd

--- src/com/biomatters/plugins/biocode/BiocodePlugin.java
+++ src/com/biomatters/plugins/biocode/BiocodePlugin.java
@@ -60,7 +60,7 @@ public class BiocodePlugin extends GeneiousPlugin {
     }
 
     public String getVersion() {
-        return "1.3.10";
+        return "1.3.11";
     }
 
     public String getMinimumApiVersion() {
