5b091a598057819232972f2d021b1ff99947d107
Steven Stones-Havas <steve@biomatters.com>
2010-02-08 03:48:51 +0000

    Moorea upping the build number
    git-svn-id: svn+ssh://svn.biomatters.com/usr/local/svnroot/geneious/trunk/plugins/MooreaLabBench@31220 84f1a7b3-ea07-0410-887d-f1e6e76816dd

--- src/com/biomatters/plugins/biocode/BiocodePlugin.java
+++ src/com/biomatters/plugins/biocode/BiocodePlugin.java
@@ -60,7 +60,7 @@ public class BiocodePlugin extends GeneiousPlugin {
     }
 
     public String getVersion() {
-        return "1.3.16";
+        return "1.3.17";
     }
 
     public String getMinimumApiVersion() {
