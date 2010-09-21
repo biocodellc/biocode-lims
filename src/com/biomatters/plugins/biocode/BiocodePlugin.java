5d2f39a048de2b0250f5043bd6f4516c2240acbc
Steven Stones-Havas <steve@biomatters.com>
2010-09-21 04:48:24 +0000

    spellings!biocode plugin version update...
    git-svn-id: svn+ssh://svn.biomatters.com/usr/local/svnroot/geneious/trunk/plugins/MooreaLabBench@36676 84f1a7b3-ea07-0410-887d-f1e6e76816dd

--- src/com/biomatters/plugins/biocode/BiocodePlugin.java
+++ src/com/biomatters/plugins/biocode/BiocodePlugin.java
@@ -66,7 +66,7 @@ public class BiocodePlugin extends GeneiousPlugin {
     }
 
     public String getVersion() {
-        return "1.6.2";
+        return "1.7";
     }
 
     public int getMaximumApiVersion() {
