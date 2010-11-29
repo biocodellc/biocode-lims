5713ee54dfcc3eba3bfdf5b083a98754200d0bcf
Richard Moir <richard@biomatters.com>
2010-11-29 02:55:27 +0000

    MBP-150 Chris wants a way to get primers from the LIMS to your local DB
    git-svn-id: svn+ssh://svn.biomatters.com/usr/local/svnroot/geneious/trunk/plugins/MooreaLabBench@38125 84f1a7b3-ea07-0410-887d-f1e6e76816dd

--- src/com/biomatters/plugins/biocode/labbench/reaction/SaveMyPrimersActionListener.java
+++ src/com/biomatters/plugins/biocode/labbench/reaction/SaveMyPrimersActionListener.java
@@ -1,38 +1,32 @@
 package com.biomatters.plugins.biocode.labbench.reaction;
 
-import com.biomatters.geneious.publicapi.documents.DocumentUtilities;
-import com.biomatters.geneious.publicapi.documents.AnnotatedPluginDocument;
-import com.biomatters.geneious.publicapi.documents.sequence.SequenceAnnotation;
-import com.biomatters.geneious.publicapi.documents.sequence.SequenceDocument;
-import com.biomatters.geneious.publicapi.plugin.*;
-import com.biomatters.geneious.publicapi.implementations.sequence.DefaultNucleotideSequence;
-import com.biomatters.geneious.publicapi.implementations.sequence.OligoSequenceDocument;
 import com.biomatters.geneious.publicapi.components.Dialogs;
-import com.biomatters.plugins.biocode.labbench.BiocodeService;
+import com.biomatters.geneious.publicapi.documents.AnnotatedPluginDocument;
+import com.biomatters.geneious.publicapi.documents.DocumentUtilities;
+import com.biomatters.geneious.publicapi.documents.sequence.SequenceDocument;
+import com.biomatters.geneious.publicapi.implementations.sequence.OligoSequenceDocument;
+import com.biomatters.geneious.publicapi.plugin.DocumentOperationException;
+import com.biomatters.geneious.publicapi.plugin.DocumentSelectionOption;
 
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.*;
-import java.util.List;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Collections;
-
-import jebl.util.ProgressListener;
+import java.util.List;
 
 /**
  * @author Steve
  * @version $Id$
  */
 public abstract class SaveMyPrimersActionListener implements ActionListener {
-    private static SequenceAnnotationGenerator PRIMER_GENERATOR;
+//    private static SequenceAnnotationGenerator PRIMER_GENERATOR;
 
     public void actionPerformed(ActionEvent e) {
-        if(PRIMER_GENERATOR == null) {
-            PRIMER_GENERATOR = PluginUtilities.getSequenceAnnotationGenerator("com.biomatters.plugins.primerDesign.PrimerCharacteristicAnnotationGenerator");
-        }
+//        if(PRIMER_GENERATOR == null) { //todo don't store this statically because plugins can be reloaded
+//            PRIMER_GENERATOR = PluginUtilities.getSequenceAnnotationGenerator("com.biomatters.plugins.primerDesign.PrimerCharacteristicAnnotationGenerator");
+//        }
 
-        Runnable runnable = new Runnable() {
-            public void run() {
+//        Runnable runnable = new Runnable() {
+//            public void run() {
                 List<AnnotatedPluginDocument> primers = new ArrayList<AnnotatedPluginDocument>();
                 for(DocumentSelectionOption option : getPrimerOptions()) {
                     for(AnnotatedPluginDocument doc : option.getValue().getDocuments()) {
@@ -41,31 +35,33 @@ public abstract class SaveMyPrimersActionListener implements ActionListener {
                 }
 
                 try {
-                    Options options = PRIMER_GENERATOR.getOptions(primers.toArray(new AnnotatedPluginDocument[primers.size()]), null);
-                    options.restoreDefaults();
-                    List<List<SequenceAnnotation>> annotations = PRIMER_GENERATOR.generateAnnotations(primers.toArray(new AnnotatedPluginDocument[primers.size()]), null, ProgressListener.EMPTY, options);
-                    if(annotations.size() == primers.size()) {
+//                    Options options = PRIMER_GENERATOR.getOptions(primers.toArray(new AnnotatedPluginDocument[primers.size()]), null);
+//                    options.restoreDefaults();
+                    //todo fix this
+//                    List<List<SequenceAnnotation>> annotations = PRIMER_GENERATOR.generateAnnotations(primers.toArray(new AnnotatedPluginDocument[primers.size()]), null, ProgressListener.EMPTY, options);
+//                    if(annotations.size() == primers.size()) {
                         List<AnnotatedPluginDocument> newPrimers = new ArrayList<AnnotatedPluginDocument>();
                         for(int i=0; i < primers.size(); i++) {
                             AnnotatedPluginDocument origDoc = primers.get(i);
                             SequenceDocument origSeq = (SequenceDocument)origDoc.getDocument();
                             OligoSequenceDocument sequence = new OligoSequenceDocument(origDoc.getName(), origDoc.getSummary(), origSeq.getSequenceString(), origDoc.getCreationDate());
-                            sequence.setAnnotations(annotations.get(i));
+//                            sequence.setAnnotations(annotations.get(i));
                             newPrimers.add(DocumentUtilities.createAnnotatedPluginDocument(sequence));
                         }
-                        primers = newPrimers;
+//                        primers = newPrimers;
+                    DocumentUtilities.addGeneratedDocuments(newPrimers, false);
 
-                    }
+//                    }
 
                 } catch (DocumentOperationException e1) {
-                    Dialogs.showMessageDialog("Could not generate primer annotations - please run \"Characteristics for Selection\" on your new primers.");
-                    DocumentUtilities.addGeneratedDocuments(primers, false, Collections.<AnnotatedPluginDocument>emptyList());
+                    Dialogs.showMessageDialog("Failed to load primer document: " + e1.getMessage());
+//                    DocumentUtilities.addGeneratedDocuments(primers, false, Collections.<AnnotatedPluginDocument>emptyList());
                 }
 
-            }
+//            }
 
-        };
-        BiocodeService.block("Extracting primers...", null, runnable);
+//        };
+//        BiocodeService.block("Extracting primers...", null, runnable);
     }
 
     public abstract List<DocumentSelectionOption> getPrimerOptions();
