d2fd5df9d37439cfddd35e70a734f4dedcb704cf
Richard Moir <richard@biomatters.com>
2011-01-10 21:11:26 +0000

    MBP-150 - Chris wants a way to get primers from the LIMS to your local DB
    Delete commented code
    
    
    git-svn-id: svn+ssh://svn.biomatters.com/usr/local/svnroot/geneious/trunk/plugins/MooreaLabBench@38656 84f1a7b3-ea07-0410-887d-f1e6e76816dd

--- src/com/biomatters/plugins/biocode/labbench/reaction/SaveMyPrimersActionListener.java
+++ src/com/biomatters/plugins/biocode/labbench/reaction/SaveMyPrimersActionListener.java
@@ -18,50 +18,26 @@ import java.util.List;
  * @version $Id$
  */
 public abstract class SaveMyPrimersActionListener implements ActionListener {
-//    private static SequenceAnnotationGenerator PRIMER_GENERATOR;
 
     public void actionPerformed(ActionEvent e) {
-//        if(PRIMER_GENERATOR == null) { //todo don't store this statically because plugins can be reloaded
-//            PRIMER_GENERATOR = PluginUtilities.getSequenceAnnotationGenerator("com.biomatters.plugins.primerDesign.PrimerCharacteristicAnnotationGenerator");
-//        }
+        List<AnnotatedPluginDocument> primers = new ArrayList<AnnotatedPluginDocument>();
+        for(DocumentSelectionOption option : getPrimerOptions()) {
+            for(AnnotatedPluginDocument doc : option.getValue().getDocuments()) {
+                primers.add(doc);
+            }
+        }
 
-//        Runnable runnable = new Runnable() {
-//            public void run() {
-                List<AnnotatedPluginDocument> primers = new ArrayList<AnnotatedPluginDocument>();
-                for(DocumentSelectionOption option : getPrimerOptions()) {
-                    for(AnnotatedPluginDocument doc : option.getValue().getDocuments()) {
-                        primers.add(doc);
-                    }
-                }
-
-                try {
-//                    Options options = PRIMER_GENERATOR.getOptions(primers.toArray(new AnnotatedPluginDocument[primers.size()]), null);
-//                    options.restoreDefaults();
-                    //todo fix this
-//                    List<List<SequenceAnnotation>> annotations = PRIMER_GENERATOR.generateAnnotations(primers.toArray(new AnnotatedPluginDocument[primers.size()]), null, ProgressListener.EMPTY, options);
-//                    if(annotations.size() == primers.size()) {
-                        List<AnnotatedPluginDocument> newPrimers = new ArrayList<AnnotatedPluginDocument>();
-                        for(int i=0; i < primers.size(); i++) {
-                            AnnotatedPluginDocument origDoc = primers.get(i);
-                            SequenceDocument origSeq = (SequenceDocument)origDoc.getDocument();
-                            OligoSequenceDocument sequence = new OligoSequenceDocument(origDoc.getName(), origDoc.getSummary(), origSeq.getSequenceString(), origDoc.getCreationDate());
-//                            sequence.setAnnotations(annotations.get(i));
-                            newPrimers.add(DocumentUtilities.createAnnotatedPluginDocument(sequence));
-                        }
-//                        primers = newPrimers;
-                    DocumentUtilities.addGeneratedDocuments(newPrimers, false);
-
-//                    }
-
-                } catch (DocumentOperationException e1) {
-                    Dialogs.showMessageDialog("Failed to load primer document: " + e1.getMessage());
-//                    DocumentUtilities.addGeneratedDocuments(primers, false, Collections.<AnnotatedPluginDocument>emptyList());
-                }
-
-//            }
-
-//        };
-//        BiocodeService.block("Extracting primers...", null, runnable);
+        try {
+            List<AnnotatedPluginDocument> newPrimers = new ArrayList<AnnotatedPluginDocument>();
+            for (AnnotatedPluginDocument origDoc : primers) {
+                SequenceDocument origSeq = (SequenceDocument) origDoc.getDocument();
+                OligoSequenceDocument sequence = new OligoSequenceDocument(origDoc.getName(), origDoc.getSummary(), origSeq.getSequenceString(), origDoc.getCreationDate());
+                newPrimers.add(DocumentUtilities.createAnnotatedPluginDocument(sequence));
+            }
+            DocumentUtilities.addGeneratedDocuments(newPrimers, false);
+        } catch (DocumentOperationException e1) {
+            Dialogs.showMessageDialog("Failed to load primer document: " + e1.getMessage());
+        }
     }
 
     public abstract List<DocumentSelectionOption> getPrimerOptions();
