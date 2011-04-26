386a69672730e9f8cd75aa83643797ae12b67583
Steven Stones-Havas <steve@biomatters.com>
2011-04-26 08:00:23 +0000

    MBP-215 support flickr photos for FIMS other than biocode
    git-svn-id: svn+ssh://svn.biomatters.com/usr/local/svnroot/geneious/trunk/plugins/MooreaLabBench@41244 84f1a7b3-ea07-0410-887d-f1e6e76816dd

--- src/com/biomatters/plugins/biocode/labbench/TissueImagesViewerFactory.java
+++ src/com/biomatters/plugins/biocode/labbench/TissueImagesViewerFactory.java
@@ -10,9 +10,14 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.*;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
+import java.net.URLEncoder;
 import java.io.InputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import com.biomatters.plugins.biocode.labbench.fims.MooreaFimsSample;
 import org.jdom.input.SAXBuilder;
@@ -46,9 +51,10 @@ public class TissueImagesViewerFactory extends DocumentViewerFactory{
 
     public DocumentViewer createViewer(final AnnotatedPluginDocument[] annotatedDocuments) {
         TissueDocument tissueDoc = (TissueDocument)annotatedDocuments[0].getDocumentOrCrash();
-        if(!MooreaFimsSample.class.isAssignableFrom(tissueDoc.getFimsSampleClass())) {
-            return null;
-        }
+        final Boolean isMooreaFims = MooreaFimsSample.class.isAssignableFrom(tissueDoc.getFimsSampleClass());
+//        if(!isMooreaFims) {
+//            return null;
+//        }
         return new DocumentViewer(){
             JPanel panel;
             public JComponent getComponent() {
@@ -80,19 +86,13 @@ public class TissueImagesViewerFactory extends DocumentViewerFactory{
                         Runnable runnable = new Runnable() {
                             public void run() {
                                 try {
-                                    URL xmlUrl = new URL("http://calphotos.berkeley.edu/cgi-bin/img_query?getthumbinfo=1&specimen_no="+doc.getSpecimenId()+"&format=xml&num=all&query_src=lims");
-                                    InputStream in = xmlUrl.openStream();
-                                    SAXBuilder builder = new SAXBuilder();
-                                    Element root = builder.build(in).detachRootElement();
-                                    XMLOutputter out = new XMLOutputter(Format.getPrettyFormat());
-                                    out.output(root, System.out);
-                                    List<Element> imageUrls = root.getChildren("enlarge_jpeg_url");
+                                    List<String> imageUrls = isMooreaFims ? getImageUrlsCalphotos(doc) : getImageUrlsFlickr(doc);
                                     if(imageUrls != null && imageUrls.size() > 0) {
                                         Image[] images = new Image[imageUrls.size()];
                                         MediaTracker m = new MediaTracker(panel);
                                         for(int i=0; i < imageUrls.size(); i++) {
-                                            URL imageUrl = new URL(imageUrls.get(i).getText());
-                                            System.out.println(imageUrl);
+                                            System.out.println(imageUrls.get(i));
+                                            URL imageUrl = new URL(imageUrls.get(i));
                                             Image img = Toolkit.getDefaultToolkit().createImage(imageUrl);
                                             images[i] = img;
                                             m.addImage(img,0);
@@ -146,6 +146,52 @@ public class TissueImagesViewerFactory extends DocumentViewerFactory{
                 return panel;
             }
         };
+    }
+
+    private List<String> getImageUrlsCalphotos(TissueDocument doc) throws IOException, JDOMException {
+        URL xmlUrl = new URL("http://calphotos.berkeley.edu/cgi-bin/img_query?getthumbinfo=1&specimen_no="+URLEncoder.encode(doc.getSpecimenId(), "UTF-8")+"&format=xml&num=all&query_src=lims");
+        InputStream in = xmlUrl.openStream();
+        SAXBuilder builder = new SAXBuilder();
+        Element root = builder.build(in).detachRootElement();
+        XMLOutputter out = new XMLOutputter(Format.getPrettyFormat());
+        out.output(root, System.out);
+        List<Element> imageUrls = root.getChildren("enlarge_jpeg_url");
+        List<String> result = new ArrayList<String>();
+        for(Element e : imageUrls) {
+            result.add(e.getText());
+        }
+        return result;
+    }
+
+
+    private List<String> getImageUrlsFlickr(TissueDocument doc) throws IOException, JDOMException {
+        URL xmlUrl = new URL("http://www.flickr.com/services/rest/?method=flickr.photos.search&format=rest&machine_tags=bioValidator:specimen="+URLEncoder.encode(doc.getSpecimenId(), "UTF-8")+"&api_key=724c92d972c3822bdb9c8ff501fb3d6a");
+        System.out.println(xmlUrl);
+        final HttpURLConnection urlConnection = (HttpURLConnection)xmlUrl.openConnection();
+        urlConnection.setRequestMethod("GET");
+        InputStream in = urlConnection.getInputStream();
+        SAXBuilder builder = new SAXBuilder();
+        Element root = builder.build(in).detachRootElement();
+        XMLOutputter out = new XMLOutputter(Format.getPrettyFormat());
+        out.output(root, System.out);
+        if(root.getName().equals("rsp") && "ok".equals(root.getAttributeValue("stat"))) {
+            root = root.getChild("photos");
+        }
+        else {
+            if(root.getChild("err") != null) {
+                throw new IOException(root.getChild("err").getAttributeValue("msg"));
+            }
+            return Collections.emptyList();
+        }
+        if(root == null) {
+            return Collections.emptyList();
+        }
+        List<Element> imageUrls = root.getChildren("photo");
+        List<String> result = new ArrayList<String>();
+        for(Element e : imageUrls) {
+            result.add("http://farm"+e.getAttributeValue("farm")+".static.flickr.com/"+e.getAttributeValue("server")+"/"+e.getAttributeValue("id")+"_"+e.getAttributeValue("secret")+"_z.jpg");
+        }
+        return result;
     }
 
 }
