092cd3a1ec0a5d81126da832fbf3a93678088542
Matthew Cheung <matthew@biomatters.com>
2013-07-26 04:47:54 +0000

    MBP-331 - Searching is SUPER DUPER SLOW!!! Fix bug: Add plates without workflows
    git-svn-id: svn+ssh://svn.biomatters.com/usr/local/svnroot/geneious/trunk/plugins/MooreaLabBench@59288 84f1a7b3-ea07-0410-887d-f1e6e76816dd

--- src/com/biomatters/plugins/biocode/labbench/lims/LIMSConnection.java
+++ src/com/biomatters/plugins/biocode/labbench/lims/LIMSConnection.java
@@ -1608,7 +1608,11 @@ public abstract class LIMSConnection {
             System.out.println("\tTook " + (System.currentTimeMillis() - start) + "ms to do LIMS query");
             Map<Integer, Plate> extraPlates = createPlateDocuments(callback, callback, remainingPlatesSet);
             for (Plate plate : extraPlates.values()) {
-                result.plates.add(new PlateDocument(plate));
+                PlateDocument plateDoc = new PlateDocument(plate);
+                if(callback != null) {
+                    callback.add(plateDoc, Collections.<String, Object>emptyMap());
+                }
+                result.plates.add(plateDoc);
             }
         }
     }
