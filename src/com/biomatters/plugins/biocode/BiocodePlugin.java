fc74ff5b432a57f97573e2dfb0a47f0f3b086be3
Matthew Cheung <matthew@biomatters.com>
2013-09-23 03:53:43 +0000

    MBP-365 Release 2.5.0 Update version number
    git-svn-id: svn+ssh://svn.biomatters.com/usr/local/svnroot/geneious/trunk/plugins/MooreaLabBench@60638 84f1a7b3-ea07-0410-887d-f1e6e76816dd

--- src/com/biomatters/plugins/biocode/BiocodePlugin.java
+++ src/com/biomatters/plugins/biocode/BiocodePlugin.java
@@ -38,7 +38,7 @@ public class BiocodePlugin extends GeneiousPlugin {
     }
 
     private static GeneiousActionOptions superBiocodeAction;
-    public static final String PLUGIN_VERSION = "2.4.5";
+    public static final String PLUGIN_VERSION = "2.5.0";
 
     public static GeneiousActionOptions getSuperBiocodeAction() {
         if (superBiocodeAction == null) {
