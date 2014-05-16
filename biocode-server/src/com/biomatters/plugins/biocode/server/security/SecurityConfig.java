1c576a97b78d751a33c39d8d8a69e49ce3df9a9d
Matthew Cheung <matthew@biomatters.com>
2014-05-16 12:02:17 +1200

    MBP-445 User authentication Only require security on our api for now

--- biocode-server/src/com/biomatters/plugins/biocode/server/security/SecurityConfig.java
+++ biocode-server/src/com/biomatters/plugins/biocode/server/security/SecurityConfig.java
@@ -47,8 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-                .anyRequest().authenticated()
+            .authorizeRequests().antMatchers("/biocode/**")
+                .authenticated()
                 .and()
             .httpBasic();
     }
