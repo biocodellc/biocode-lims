509d08295c33d360d80bbed3a8f720dc4ef040ff
Matthew Cheung <matthew@biomatters.com>
2014-05-19 15:04:37 +1200

    MBP-445 User authentication Disable Spring Security's automatic CSRF protection

--- biocode-server/src/com/biomatters/plugins/biocode/server/security/SecurityConfig.java
+++ biocode-server/src/com/biomatters/plugins/biocode/server/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
             .authorizeRequests().antMatchers("/biocode/**")
                 .authenticated()
                 .and()
