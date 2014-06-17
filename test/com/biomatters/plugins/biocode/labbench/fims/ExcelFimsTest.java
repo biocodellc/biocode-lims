be1a2dbfd12a12acaaa61470d15f4d9a27399890
Gen Li <gen@biomatters.com>
2014-06-17 14:20:12 +1200

    MBP-483 - Excel FIMS connection always scanning first column for duplicates instead of the tissue column

--- test/com/biomatters/plugins/biocode/labbench/fims/ExcelFimsTest.java
+++ test/com/biomatters/plugins/biocode/labbench/fims/ExcelFimsTest.java
@@ -13,21 +13,21 @@ import org.junit.Test;
  * Created Gen Li on 28/04/14.
  */
 public class ExcelFimsTest extends Assert {
-    @Test(expected = ConnectionException.class)
-    public void failsOnDuplicateKeys() throws ConnectionException {
-        TestGeneious.initialize();
-        ExcelFimsConnection connection = new ExcelFimsConnection();
-        TableFimsConnectionOptions options = connection._getConnectionOptions();
-        options.setValue(ExcelFimsConnectionOptions.CONNECTION_OPTIONS_KEY + "." +ExcelFimsConnectionOptions.FILE_LOCATION, TestUtilities.getPathToDemoFIMSExcel(ExcelFimsTest.class, "invalid demo video FIMS.xls"));
-        connection._connect(options);
-    }
-
     @Test
     public void successOnNormalConnectionAttempt() throws ConnectionException {
         TestGeneious.initialize();
         ExcelFimsConnection connection = new ExcelFimsConnection();
         TableFimsConnectionOptions options = connection._getConnectionOptions();
         options.setValue(ExcelFimsConnectionOptions.CONNECTION_OPTIONS_KEY + "." +ExcelFimsConnectionOptions.FILE_LOCATION, TestUtilities.getPathToDemoFIMSExcel(ExcelFimsTest.class, "demo video FIMS.xls"));
+        connection._connect(options);
+    }
+
+    @Test(expected = ConnectionException.class)
+    public void failsOnDuplicateKeys() throws ConnectionException {
+        TestGeneious.initialize();
+        ExcelFimsConnection connection = new ExcelFimsConnection();
+        TableFimsConnectionOptions options = connection._getConnectionOptions();
+        options.setValue(ExcelFimsConnectionOptions.CONNECTION_OPTIONS_KEY + "." +ExcelFimsConnectionOptions.FILE_LOCATION, TestUtilities.getPathToDemoFIMSExcel(ExcelFimsTest.class, "demo video FIMS with duplicate tissue ids.xls"));
         connection._connect(options);
     }
 }
