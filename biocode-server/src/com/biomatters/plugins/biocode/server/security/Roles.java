280d0774c11acf70ed1804b2c87892a69ccc1e14
Matthew Cheung <matthew@biomatters.com>
2014-06-30 12:05:44 +1200

    MBP-447 - Project Roles with data access restrictions Add a service to retrieve the roles in the system.

--- biocode-server/src/com/biomatters/plugins/biocode/server/security/Roles.java
+++ biocode-server/src/com/biomatters/plugins/biocode/server/security/Roles.java
@@ -1,8 +1,6 @@
 package com.biomatters.plugins.biocode.server.security;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -17,5 +15,17 @@ public class Roles {
     @Produces({"application/json;qs=1", "application/xml;qs=0.5"})
     public Response list() {
         return Response.ok(new GenericEntity<List<Role>>(Role.values()) { }).build();
+    }
+
+    @GET
+    @Produces({"application/json;qs=1", "application/xml;qs=0.5"})
+    @Path("{name}")
+    public Role forName(@PathParam("name")String name) {
+        for (Role role : Role.values()) {
+            if(role.name.equalsIgnoreCase(name)) {
+                return role;
+            }
+        }
+        throw new NotFoundException("No role for " + name);
     }
 }
