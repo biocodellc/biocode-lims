f263c31b1b413bc1a193fd3d95378407a58bebe8
Matthew Cheung <matthew@biomatters.com>
2014-08-18 13:51:07 +1200

    MBP-547 - Release 2.8.0

--- src/com/biomatters/plugins/biocode/BiocodePlugin.java
+++ src/com/biomatters/plugins/biocode/BiocodePlugin.java
@@ -41,7 +41,7 @@ public class BiocodePlugin extends GeneiousPlugin {
     }
 
     private static GeneiousActionOptions superBiocodeAction;
-    public static final String PLUGIN_VERSION = "2.99.1";
+    public static final String PLUGIN_VERSION = "2.8.0";
 
     public static GeneiousActionOptions getSuperBiocodeAction() {
         if (superBiocodeAction == null) {
