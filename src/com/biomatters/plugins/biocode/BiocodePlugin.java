07180e7efe1b1938a71b60a420c1b97d3cda1491
Matthew Cheung <matthew@biomatters.com>
2014-12-12 11:45:07 +1300

    MBP-661 - Release 2.8.6 Increment version number for next release

--- src/com/biomatters/plugins/biocode/BiocodePlugin.java
+++ src/com/biomatters/plugins/biocode/BiocodePlugin.java
@@ -41,7 +41,7 @@ public class BiocodePlugin extends GeneiousPlugin {
     }
 
     private static GeneiousActionOptions superBiocodeAction;
-    public static final String PLUGIN_VERSION = "2.8.6";
+    public static final String PLUGIN_VERSION = "2.8.7";
 
     public static GeneiousActionOptions getSuperBiocodeAction() {
         if (superBiocodeAction == null) {
