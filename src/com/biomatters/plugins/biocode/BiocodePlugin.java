24238fa856e991df4f0c0236f62ab3eab8516317
Gen Li <gen@biomatters.com>
2015-01-14 18:52:13 +1300

    MBP-668 - Release Biocode 2.8.7
    Updated the version of the BiocodePlugin to 2.8.8.

--- src/com/biomatters/plugins/biocode/BiocodePlugin.java
+++ src/com/biomatters/plugins/biocode/BiocodePlugin.java
@@ -41,7 +41,7 @@ public class BiocodePlugin extends GeneiousPlugin {
     }
 
     private static GeneiousActionOptions superBiocodeAction;
-    public static final String PLUGIN_VERSION = "2.8.7";
+    public static final String PLUGIN_VERSION = "2.8.8";
 
     public static GeneiousActionOptions getSuperBiocodeAction() {
         if (superBiocodeAction == null) {
