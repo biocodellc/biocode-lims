8eac0040556a7b41ac0eecb75709d2d39ca59f75
Gen Li <gen@biomatters.com>
2015-03-02 14:56:01 +1300

    MBP-692 - Release Biocode LIMS 2.8.8
    Modified BiocodePlugin Version.

--- src/com/biomatters/plugins/biocode/BiocodePlugin.java
+++ src/com/biomatters/plugins/biocode/BiocodePlugin.java
@@ -41,7 +41,7 @@ public class BiocodePlugin extends GeneiousPlugin {
     }
 
     private static GeneiousActionOptions superBiocodeAction;
-    public static final String PLUGIN_VERSION = "2.8.8";
+    public static final String PLUGIN_VERSION = "2.8.9";
     public static final String SUPPORT_EMAIL = "support@mooreabiocode.org";
 
     public static GeneiousActionOptions getSuperBiocodeAction() {
