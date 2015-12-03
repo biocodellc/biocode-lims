d71e1e00e0cd283c0cb81b29d054576ab239afb1
Matthew Cheung <matthew@biomatters.com>
2015-12-04 10:23:59 +1100

    Increment version for release to SI

--- src/com/biomatters/plugins/biocode/BiocodePlugin.java
+++ src/com/biomatters/plugins/biocode/BiocodePlugin.java
@@ -41,7 +41,7 @@ public class BiocodePlugin extends GeneiousPlugin {
     }
 
     private static GeneiousActionOptions superBiocodeAction;
-    public static final String PLUGIN_VERSION = "2.9.1";
+    public static final String PLUGIN_VERSION = "2.9.2";
     public static final String SUPPORT_EMAIL = "support@mooreabiocode.org";
 
     public static GeneiousActionOptions getSuperBiocodeAction() {
