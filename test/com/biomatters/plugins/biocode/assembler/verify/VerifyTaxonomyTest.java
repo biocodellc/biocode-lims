060b4f1d0d536b38516e38e369541ce502a715fb
Jonas Kuhn <jonas@biomatters.com>
2016-11-18 12:18:10 +1300

    MBP-777 Biocode Plugin 3.0.1: RuntimeException in Options$Option.<init> (group 12501)
    - added a test

---
+++ test/com/biomatters/plugins/biocode/assembler/verify/VerifyTaxonomyTest.java
@@ -0,0 +1,43 @@
+package com.biomatters.plugins.biocode.assembler.verify;
+
+import com.biomatters.geneious.publicapi.documents.AnnotatedPluginDocument;
+import com.biomatters.geneious.publicapi.documents.DocumentField;
+import com.biomatters.geneious.publicapi.documents.DocumentUtilities;
+import com.biomatters.geneious.publicapi.implementations.sequence.DefaultNucleotideSequence;
+import com.biomatters.geneious.publicapi.plugin.DocumentOperationException;
+import com.biomatters.geneious.publicapi.plugin.TestGeneious;
+import jebl.util.ProgressListener;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class VerifyTaxonomyTest {
+
+    @Before
+    public void setUp() {
+        TestGeneious.initialize();
+        TestGeneious.initializeAllPlugins();
+    }
+
+    @Test
+    public void verifyTaxonomy_runDefaultBlast_resultDocumentContainsTaxonomy() throws DocumentOperationException {
+        // This test doesn't test exactly whether the default database is correct, but at least we know that it works at all
+        String sequence = "GAAGTCGTAACAAGGTAGCCGTATCGGAAGGTGCGGCTGGATCACCTCCTTTCTAAGGAAAAGGAAACCTGTGAGTTTTCGTTCTTCT" +
+                "CTATTTGTTCAGTTTTGAGAGGTTAGTACTTCTCAGTATGTTTGTTCTTTGAAAACTAGATAAGAAAGTTAGTAAAGTTAGCATAGATAATTTATTAT" +
+                "TTATGACACAAGTAACCGAGAATCATCTGAAAGTGAATCTTTCATCTGATTGGATGTATCATCGCTGATACGGAAAATCAGAAAAACAACCTTTACTT" +
+                "CGTAGAAGTAAATTGGTTAAGTTAGAAAGGGCGCACGGTGGATGCCTTG";
+        DefaultNucleotideSequence someFirmicutes = new DefaultNucleotideSequence("KC179818", sequence);
+        String taxonomy = "Bacteria; Firmicutes; Bacilli; Bacillales; Listeriaceae; Listeria";
+        someFirmicutes.setFieldValue(DocumentField.TAXONOMY_FIELD, taxonomy);
+        AnnotatedPluginDocument query = DocumentUtilities.createAnnotatedPluginDocument(someFirmicutes);
+        AnnotatedPluginDocument[] documents = {query};
+        VerifyTaxonomyOptions options = new VerifyTaxonomyOptions(documents);
+        List<AnnotatedPluginDocument> results = new VerifyTaxonomyOperation().performOperation(documents, ProgressListener.EMPTY, options);
+        List<AnnotatedPluginDocument> hitDocuments = ((VerifyTaxonomyResultsDocument) results.get(0).getDocument()).getResults().get(0).hitDocuments;
+        assertEquals(5, hitDocuments.size());
+        assertEquals(taxonomy, hitDocuments.get(0).getFieldValue(DocumentField.TAXONOMY_FIELD));
+    }
+}
