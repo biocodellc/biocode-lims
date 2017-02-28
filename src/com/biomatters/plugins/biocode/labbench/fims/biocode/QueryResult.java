021579865fe3be351dafc55850ecf0d2af7386a9
Frank Lee <frank@biomatters.com>
2017-02-28 19:32:41 +1300

    MBP-785 Biocode Plugin: Update to use new query system
    add missed files

---
+++ src/com/biomatters/plugins/biocode/labbench/fims/biocode/QueryResult.java
@@ -0,0 +1,111 @@
+
+package com.biomatters.plugins.biocode.labbench.fims.biocode;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class QueryResult {
+
+    @SerializedName("content")
+    @Expose
+    private List<Content> content = null;
+    @SerializedName("last")
+    @Expose
+    private Boolean last;
+    @SerializedName("totalPages")
+    @Expose
+    private Integer totalPages;
+    @SerializedName("totalElements")
+    @Expose
+    private Integer totalElements;
+    @SerializedName("sort")
+    @Expose
+    private Object sort;
+    @SerializedName("first")
+    @Expose
+    private Boolean first;
+    @SerializedName("numberOfElements")
+    @Expose
+    private Integer numberOfElements;
+    @SerializedName("size")
+    @Expose
+    private Integer size;
+    @SerializedName("number")
+    @Expose
+    private Integer number;
+
+    public List<Content> getContent() {
+        return content;
+    }
+
+    public void setContent(List<Content> content) {
+        this.content = content;
+    }
+
+    public Boolean getLast() {
+        return last;
+    }
+
+    public void setLast(Boolean last) {
+        this.last = last;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public Integer getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(Integer totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public Object getSort() {
+        return sort;
+    }
+
+    public void setSort(Object sort) {
+        this.sort = sort;
+    }
+
+    public Boolean getFirst() {
+        return first;
+    }
+
+    public void setFirst(Boolean first) {
+        this.first = first;
+    }
+
+    public Integer getNumberOfElements() {
+        return numberOfElements;
+    }
+
+    public void setNumberOfElements(Integer numberOfElements) {
+        this.numberOfElements = numberOfElements;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+}
