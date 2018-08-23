ddbacc95af88aa623ec9c16f638124a0d300ba42
John Deck <jdeck88@gmail.com>
2018-08-23 17:41:21 +1200

    adding a geomeDataProcessor to parse JSON responses and return arrayList of rows

---
+++ src/com/biomatters/plugins/biocode/labbench/fims/geome/Row.java
@@ -0,0 +1,18 @@
+package com.biomatters.plugins.biocode.labbench.fims.geome;
+
+public class Row {
+
+
+       // Event
+       public String country = "";
+       public String yearCollected = "";
+       // Sample
+       public String family = "";
+       public String phylum = "";
+       // Tissue
+       public String tissueID = "";
+       public String tissuePlate = "";
+       public String tissueWell = "";
+       public Row() {
+               }
+   }
