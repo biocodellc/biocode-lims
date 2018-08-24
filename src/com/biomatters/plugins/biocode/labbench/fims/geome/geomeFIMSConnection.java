e3b088e24336368ceb8e3ca05ce06571ec65737c
Steve Stones-Havas <steve@biomatters.com>
2018-08-24 17:47:03 +1200

    Got rid of the result caching in the GeomeFimsCOnnection

--- src/com/biomatters/plugins/biocode/labbench/fims/geome/geomeFIMSConnection.java
+++ src/com/biomatters/plugins/biocode/labbench/fims/geome/geomeFIMSConnection.java
@@ -1,6 +1,7 @@
 package com.biomatters.plugins.biocode.labbench.fims.geome;
 
 import com.biomatters.geneious.publicapi.databaseservice.*;
+import com.biomatters.geneious.publicapi.documents.Condition;
 import com.biomatters.geneious.publicapi.documents.DocumentField;
 import com.biomatters.geneious.publicapi.plugin.Options;
 import com.biomatters.geneious.publicapi.utilities.StringUtilities;
@@ -8,12 +9,16 @@ import com.biomatters.plugins.biocode.labbench.ConnectionException;
 import com.biomatters.plugins.biocode.labbench.FimsSample;
 import com.biomatters.plugins.biocode.labbench.PasswordOptions;
 import com.biomatters.plugins.biocode.labbench.TissueDocument;
+import com.biomatters.plugins.biocode.labbench.connection.Connection;
 import com.biomatters.plugins.biocode.labbench.fims.FIMSConnection;
 import com.biomatters.plugins.biocode.labbench.fims.FimsProject;
 import com.biomatters.plugins.biocode.labbench.fims.TableFimsSample;
 
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.lang.ref.SoftReference;
 import java.util.*;
@@ -69,16 +74,15 @@ public class geomeFIMSConnection extends FIMSConnection {
                 List<String> taxonomyFieldNames = Arrays.asList("urn:kingdom", "urn:phylum", "urn:subphylum", "urn:superClass", "urn:class", "urn:infraClass", "urn:subclass", "urn:superOrder", "urn:order", "urn:infraOrder", "urn:suborder", "urn:superFamily", "urn:family", "urn:subfamily", "urn:genus", "urn:subGenus", "urn:tribe", "urn:subTribe", "urn:species", "urn:subSpecies");
 
                 for (ProjectConfig.Entity entity : config.entities) {
-                    if(!Arrays.asList("Tissue", "Event", "Sample").contains(entity.conceptAlias)) {
+                    if (!Arrays.asList("Tissue", "Event", "Sample").contains(entity.conceptAlias)) {
                         continue;
                     }
 
                     for (Project.Field attribute : entity.attributes) {
                         allAttributes.put(attribute.uri, attribute.asDocumentField());
-                        if(taxonomyFieldNames.contains(attribute.uri)) {
+                        if (taxonomyFieldNames.contains(attribute.uri)) {
                             taxonomyAttributes.put(attribute.uri, attribute.asDocumentField());
-                        }
-                        else {
+                        } else {
                             collectionAttributes.put(attribute.uri, attribute.asDocumentField());
                         }
 
@@ -103,6 +107,7 @@ public class geomeFIMSConnection extends FIMSConnection {
     private static final String TISSUE_URN = "urn:tissueID";
     private static final String SAMPLE_URN = "urn:materialSampleID";
     private static final String EVENT_ID = "eventID";
+
     @Override
     public DocumentField getTissueSampleDocumentField() {
         return allAttributes.get(TISSUE_URN);
@@ -138,17 +143,14 @@ public class geomeFIMSConnection extends FIMSConnection {
         return getTissueIdsMatchingQuery(query, projectsToMatch, true);
     }
 
-    private Map<String, SoftReference<Map<String, Object>>> cachedTissues = new HashMap<>();
-    private Map<String, SoftReference<Map<String, Object>>> cachedSamples = new HashMap<>();
-    private Map<String, SoftReference<Map<String, Object>>> cachedEvents = new HashMap<>();
-
     @Override
     public List<String> getTissueIdsMatchingQuery(Query query, List<FimsProject> projectsToMatch, boolean allowEmptyQuery) throws ConnectionException {
-        String queryString = buildQuery(query) + " _select_:[Tissue,Sample,Event]";
+        String queryString = buildQuery(query);
         System.out.println(queryString);
         Invocation.Builder searchRequest = client.getQueryTarget().path("records/Tissue/json")
                 .queryParam("projectId", 3)
                 .queryParam("entity", "Tissue")
+                .queryParam("limit", 10000)
                 .queryParam("q", queryString)
                 .request();
         Response response = searchRequest.get();
@@ -156,9 +158,9 @@ public class geomeFIMSConnection extends FIMSConnection {
             List<String> ids = new ArrayList<>();
             SearchResult result = geomeFIMSClient.getRestServiceResult(SearchResult.class, response);
 
-            cacheMapById(getTissueSampleDocumentField().getName(), result.content.Tissue, cachedTissues, ids);
-            cacheMapById("materialSampleID", result.content.Sample, cachedSamples, ids);
-            cacheMapById(EVENT_ID, result.content.Event, cachedEvents, ids);
+            for (Map<String, Object> tissue : result.content.Tissue) {
+                ids.add(tissue.get(getTissueSampleDocumentField().getName()).toString());
+            }
 
             return ids;
         } catch (DatabaseServiceException e) {
@@ -167,14 +169,13 @@ public class geomeFIMSConnection extends FIMSConnection {
     }
 
     private String buildQuery(Query query) {
-        if(query instanceof BasicSearchQuery) {
+        if (query instanceof BasicSearchQuery) {
             return ((BasicSearchQuery) query).getSearchText();
-        }
-        else if(query instanceof CompoundSearchQuery) {
-            CompoundSearchQuery cquery = (CompoundSearchQuery)query;
+        } else if (query instanceof CompoundSearchQuery) {
+            CompoundSearchQuery cquery = (CompoundSearchQuery) query;
             CompoundSearchQuery.Operator operator = cquery.getOperator();
             String join = "";
-            switch(operator) {
+            switch (operator) {
                 case OR:
                     join = " OR ";
                     break;
@@ -187,16 +188,13 @@ public class geomeFIMSConnection extends FIMSConnection {
                 childQueries.add(buildQuery(childQuery));
             }
             return StringUtilities.join(join, childQueries);
-        }
-        else if(query instanceof AdvancedSearchQueryTerm) {
-            return getQueryExpression((AdvancedSearchQueryTerm)query);
-        }
-        else {
-            throw new RuntimeException("Unrecognised query type: "+query.getClass());
+        } else if (query instanceof AdvancedSearchQueryTerm) {
+            return getQueryExpression((AdvancedSearchQueryTerm) query);
+        } else {
+            throw new RuntimeException("Unrecognised query type: " + query.getClass());
         }
 
     }
-
 
 
     public static String getQueryExpression(AdvancedSearchQueryTerm query) {
@@ -204,18 +202,18 @@ public class geomeFIMSConnection extends FIMSConnection {
         String append = "\"";
         String prepend = "\"";
         String beforeQuery = "";
-        switch(query.getCondition()) {
+        switch (query.getCondition()) {
             case EQUAL:
                 join = "::";
                 break;
             case APPROXIMATELY_EQUAL:
                 join = ":";
-                prepend="\"%";
+                prepend = "\"%";
                 append = "%\"";
                 break;
             case BEGINS_WITH:
                 join = "::";
-                append="%\"";
+                append = "%\"";
                 break;
             case ENDS_WITH:
                 join = "::";
@@ -263,19 +261,17 @@ public class geomeFIMSConnection extends FIMSConnection {
             case IN_RANGE:
                 //todo: this is a special case
                 return query.getField().getName() + ":[" + query.getValues()[0] + " TO " + query.getValues()[1] + "]";
-         }
+        }
         return beforeQuery + query.getField().getName() + join + prepend + query.getValues()[0] + append;
     }
 
-    private void cacheMapById(String idField, List<Map<String, Object>> listToCache, Map<String, SoftReference<Map<String, Object>>> cache, List<String> idList) {
-        for (Map<String, Object> tissueMap : listToCache) {
-
+    private Map<String, Map<String, Object>> mapResults(String idField, List<Map<String, Object>> listToMap) {
+        Map<String, Map<String, Object>> result = new HashMap<>();
+        for (Map<String, Object> tissueMap : listToMap) {
             String id = tissueMap.get(idField).toString();
-            cache.put(id, new SoftReference<>(tissueMap));
-            if(idList != null) {
-                idList.add(id);
-            }
+            result.put(id, tissueMap);
         }
+        return result;
     }
 
     @Override
@@ -283,93 +279,118 @@ public class geomeFIMSConnection extends FIMSConnection {
         Map<String, DocumentField> attributesByName = new HashMap<>();
         allAttributes.values().forEach(f -> attributesByName.put(f.getName(), f));
 
-        List<FimsSample> samples = new ArrayList<>();
+        Query[] tissueQueries = new Query[tissueIds.size()];
+        for (int i = 0; i < tissueIds.size(); i++) {
+            tissueQueries[i] = Query.Factory.createFieldQuery(getTissueSampleDocumentField(), Condition.EQUAL, tissueIds.get(i));
+        }
+        Query tissueQuery = Query.Factory.createOrQuery(tissueQueries, Collections.emptyMap());
 
-        List<String> uncachedTissueIds = new ArrayList<>();
+        String queryString = buildQuery(tissueQuery);
 
-        for (String tissueId : tissueIds) {
-            Map<String, Object> valuesForTissue = null;
-            SoftReference<Map<String, Object>> ref = cachedTissues.get(tissueId);
-            if(ref != null) {
-                valuesForTissue = ref.get();
-            }
+        Invocation.Builder searchRequest = client.getQueryTarget().path("records/Tissue/json")
+                .queryParam("projectId", 3)
+                .queryParam("entity", "Tissue")
+                .queryParam("limit", 10000)
+//                .queryParam("q", queryString + "_select_:[Tissue,Sample,Event]")
+                .request();
 
-            if(valuesForTissue != null) {
-                // Need to convert map from name -> value to uri -> value because that's what Geneious expects
-                Map<String, Object> valuesByCode = new HashMap<>();
 
-                BiConsumer<String, Object> storeByCode = (key, value) -> {
-                    if(value == null || value instanceof String && value.toString().trim().length() == 0) {
-                        return;
-                    }
+        Form formToPost = new Form()
+                .param("query", queryString + "_select_:[Tissue,Sample,Event]");
 
-                    DocumentField documentField = attributesByName.get(key);
-                    if (documentField != null) {
+        Response response = searchRequest.post(
+                Entity.entity(formToPost, MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+        try {
+            SearchResult result = geomeFIMSClient.getRestServiceResult(SearchResult.class, response);
 
-                        Object valueToStore;
-                        try {
-                            if(Boolean.class == documentField.getValueType()) {
-                                valueToStore = Boolean.valueOf(value.toString());
-                            } else if(Double.class == documentField.getValueType()) {
-                                valueToStore = Double.valueOf(value.toString());
-                            } else if(Integer.class == documentField.getValueType()) {
-                                valueToStore = Integer.valueOf(value.toString());
-                            } else {
-                                valueToStore = value.toString();
-                            }
-                            valuesByCode.put(documentField.getCode(), valueToStore);
-                        } catch (NumberFormatException e) {
-                            System.out.println("Invalid value for " + documentField.getValueType() + " was " + value);
+            List<FimsSample> samples = new ArrayList<>();
+
+            Map<String, Map<String, Object>> mappedTissues = mapResults(getTissueSampleDocumentField().getName(), result.content.Tissue);
+            Map<String, Map<String, Object>> mappedSamples = mapResults("materialSampleID", result.content.Sample);
+            Map<String, Map<String, Object>> mappedEvents = mapResults(EVENT_ID, result.content.Event);
+
+
+            for (String tissueId : tissueIds) {
+                Map<String, Object> valuesForTissue = mappedTissues.get(tissueId);
+
+                if (valuesForTissue != null) {
+                    // Need to convert map from name -> value to uri -> value because that's what Geneious expects
+                    Map<String, Object> valuesByCode = new HashMap<>();
+
+                    BiConsumer<String, Object> storeByCode = (key, value) -> {
+                        if (value == null || value instanceof String && value.toString().trim().length() == 0) {
+                            return;
                         }
-                    } else {
-                        // todo bring in projectId, bcid, expeditionCode from other entities
+
+                        DocumentField documentField = attributesByName.get(key);
+                        if (documentField != null) {
+
+                            Object valueToStore;
+                            try {
+                                if (Boolean.class == documentField.getValueType()) {
+                                    valueToStore = Boolean.valueOf(value.toString());
+                                } else if (Double.class == documentField.getValueType()) {
+                                    valueToStore = Double.valueOf(value.toString());
+                                } else if (Integer.class == documentField.getValueType()) {
+                                    valueToStore = Integer.valueOf(value.toString());
+                                } else {
+                                    valueToStore = value.toString();
+                                }
+                                valuesByCode.put(documentField.getCode(), valueToStore);
+                            } catch (NumberFormatException e) {
+                                System.out.println("Invalid value for " + documentField.getValueType() + " was " + value);
+                            }
+                        } else {
+                            // todo bring in projectId, bcid, expeditionCode from other entities
 //                        System.out.println("missing DocumentField for " + key);
-                    }
-                };
-                valuesForTissue.forEach(storeByCode);
-                Object sampleId = valuesForTissue.get(allAttributes.get(SAMPLE_URN).getName());
-                String eventId = null;
-                if(sampleId != null) {
-                    SoftReference<Map<String, Object>> sampleInfo = cachedSamples.get(sampleId.toString());
-                    if (sampleInfo != null) {
-                        Map<String, Object> sampleValues = sampleInfo.get();
+                        }
+                    };
+                    valuesForTissue.forEach(storeByCode);
+                    Object sampleId = valuesForTissue.get(allAttributes.get(SAMPLE_URN).getName());
+                    String eventId = null;
+                    if (sampleId != null) {
+                        Map<String, Object> sampleValues = mappedSamples.get(sampleId.toString());
+
                         if (sampleValues != null) {
                             sampleValues.forEach(storeByCode);
                             eventId = sampleValues.get(EVENT_ID).toString();
                         }
+                        else {
+                            throw new ConnectionException("Expected to find sample "+sampleId+" but it was not returned by the server");
+                        }
+
+                    }
+
+                    Map<String, Object> eventValues = mappedEvents.get(eventId);
+                    if (eventValues != null) {
+                        eventValues.forEach(storeByCode);
                     }
                     else {
-                        uncachedTissueIds.add(tissueId);
+                        throw new ConnectionException("Expected to find event " + eventId + " but it was not returned by the server");
+                    }
+
+
+                    TissueDocument sample = new TissueDocument(
+                            new TableFimsSample(
+                                    getCollectionAttributes(),
+                                    getTaxonomyAttributes(), valuesByCode,
+                                    TISSUE_URN,
+                                    SAMPLE_URN)
+                    );
+                    samples.add(sample);
+                    if (callback != null) {
+                        callback.add(sample, Collections.emptyMap());
                     }
                 }
                 else {
-                    uncachedTissueIds.add(tissueId);
-                }
-
-                SoftReference<Map<String, Object>> eventInfo = cachedEvents.get(eventId);
-                if(eventInfo != null) {
-                    Map<String, Object> eventValues = eventInfo.get();
-                    if(eventValues != null) {
-                        eventValues.forEach(storeByCode);
-                    }
-                }
-
-
-                TissueDocument sample = new TissueDocument(
-                        new TableFimsSample(
-                                getCollectionAttributes(),
-                                getTaxonomyAttributes(), valuesByCode,
-                                TISSUE_URN,
-                                SAMPLE_URN)
-                );
-                samples.add(sample);
-                if(callback != null) {
-                    callback.add(sample, Collections.emptyMap());
+                    throw new ConnectionException("Expected to find tissue "+tissueId+" but it was not returned by the server");
                 }
             }
-        }
 
-        return samples;
+            return samples;
+        } catch (DatabaseServiceException e) {
+            throw new ConnectionException(e);
+        }
     }
 
     @Override
